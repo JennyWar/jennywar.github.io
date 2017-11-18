0df26794a3b9bbec5101d031165ba66a80c1d6ee
Jennifer Guerra <jennywar@Jennifer-Guerras-MacBook-Pro.local>
2017-11-18 00:39:30 -0800

    added some javascript

--- Week3Homework/assets/javascript/game.js
+++ Week3Homework/assets/javascript/game.js
@@ -5,38 +5,72 @@ prompt("Press any key to start!")
 
 //list of superheros //
 
-var superHeroArray = ['batman', 'superman', 'wonderwoman', 
+var superHero = ['batman', 'superman', 'wonderwoman', 
 'spiderman', 'wolverine', 'thor', 'antman', 'deadpool', 'hulk', 'robin', 'punisher', 'elektra', 'daredevil'];
 
 // computer will pick a random superhero from the list //
 
-var superHero = superHeroArray[Math.floor(Math.random() * superHeroArray.length)];
+var superPerson = superHero[Math.floor(Math.random() * superHero.length)];
 
-console.log(superHero)
+console.log(superPerson)
 
 // global variables //
 
-var h;
-var count = 0;
+var currentGuess;
+var click;
+var ghostLtr;
+var record;
+var guessesRemaining;
+var winner = 0;
+var alphabetUsed = [];
+var lettersUsed = [];
 var answerArray = [];
+
 
 // underscores will match the randomly selected word//
 
-var superHero = superHero.length;
-var underscores = '';
-for (i = 0; i < superHero; i++) {
-	underscores = underscores + '_'
-};
+for (var i = 0; i < superPerson.length; i++) {
+		answerArray[i] = "_";
+		console.log(answerArray);
+	}
 
-console.log(superHero.length);
+document.getElementById('ghostLtr').textContent = answerArray.join(" ");
+
+// track which letters the user has guessed //
+
+document.onkeyup = function(event) {
+	alphabetUsed = event.key;
+	console.log(alphabetUsed);
+	lettersUsed.push(alphabetUsed);
+	document.getElementById('alphabetUsed').innerHTML = lettersUsed;
 
 
-
-function reWriteStats() {
-	console.log("-------------");
-	console.log('startUp');
 }
 
-// 
+// if the players guess matches a letter of the superheros//
+
+if(superPerson.indexOf(alphabetUsed) > -1) {
+	for(var i = 0; i < superPerson.length; i++) {
+		if(superPerson[i] === alphabetUsed) {
+			answerArray[i] = alphabetUsed;
+			document.getElementById('ghostLtr').innerHTML = alphabetUsed.join('_');
+		}
+	}
+}
+
+// to reset the game //
+
+// var reset = function () {
+// 	var totalgueses = 9;
+// 	var guesleft = 9;
+// 	guessedletter = [];
+
+// 	updateLetterToGuess();
+// 	updateGuessesLeft();
+// 	updatewins ();
+
+
+
+// }
 
 

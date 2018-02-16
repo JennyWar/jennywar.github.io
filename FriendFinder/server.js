0f2aecfcbbbd973052b203ac62aa07e52bfba504
Jennifer Guerra <jennywar@Jennifers-MacBook-Pro.local>
2018-02-15 18:21:57 -0800

    started working on survey questions

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -1,4 +1,4 @@
-// require npm packages
+// Dependencies
 // ======================================
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -16,20 +16,30 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// app.use(function (req, res) {
+//     res.setHeader('Content-Type', 'text/plain')
+//     res.write('you posted:\n')
+//     res.end(JSON.stringify(req.body, null, 2))
+// })
+
 
 // // Static files
-// // needs to be called before the routes in order to work
-// app.use(express.static('app/public'));
+// needs to be called before the routes in order to work
+app.use(express.static('app/public'));
 
 // Routes
 // ===========================================
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
-
 // Start the server to begin listening
 //===============================================
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
+
+
+
+
 

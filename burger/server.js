38fcb7c3f9c70e83ff6d1711f862672c630ec377
Jennifer Guerra <jennywar@Jennifers-MacBook-Pro.local>
2018-02-18 19:38:20 -0800

    started working on the main handlebars page

--- burger/server.js
+++ burger/server.js
@@ -21,10 +21,10 @@ app.use(bodyParser.json());
 // ==========================
 // Express-Handlebars
 // ==========================
-const handleBars = require('express-handlebars');
+const exphbs = require("express-handlebars");
 
-app.engine('handlebars', handleBars({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 
 
